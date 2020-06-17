e519d931d845e63c74663886b3b12dd951e46bbd
wowwm <67156743@qq.com>
2020-06-17 14:32:21 +0800

    bug fix

--- miniprogram/pages/todoinfo/todoinfo.js
+++ miniprogram/pages/todoinfo/todoinfo.js
@@ -19,7 +19,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // console.log(options)
+    console.log(options)
     this.pageData.id=options.id
     todos.doc(options.id).get().then(res => {
       console.log(res)
